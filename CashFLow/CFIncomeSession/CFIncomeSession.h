4c0e8cf2a39cea4a449b05508913cb00abb1f45b
Sam Watson <sam@toomuch.com.au>
2013-07-19 22:55:39 +0800

    Added CFIncomeSession

---
+++ CashFLow/CFIncomeSession/CFIncomeSession.h
@@ -0,0 +1,13 @@
+//
+//  CFIncomeSession.h
+//  CashFLow
+//
+//  Created by Sam Watson on 19/07/13.
+//  Copyright (c) 2013 Sam Watson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CFIncomeSession : NSObject
+
+@end
---
+++ CashFLow/CFIncomeSession/CFIncomeSession.m
@@ -0,0 +1,13 @@
+//
+//  CFIncomeSession.m
+//  CashFLow
+//
+//  Created by Sam Watson on 19/07/13.
+//  Copyright (c) 2013 Sam Watson. All rights reserved.
+//
+
+#import "CFIncomeSession.h"
+
+@implementation CFIncomeSession
+
+@end
