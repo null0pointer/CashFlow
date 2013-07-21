ba732e8987e18abaa11838c1951dfc95b7c2b2e3
Sam Watson <sam@toomuch.com.au>
2013-07-22 04:58:19 +0800

    Added CFSavingsGoalEditView

---
+++ CashFLow/SavingsGoal/CFSavingsGoalEditView.h
@@ -0,0 +1,13 @@
+//
+//  CFSavingsGoalEditView.h
+//  CashFLow
+//
+//  Created by Sam Watson on 22/07/13.
+//  Copyright (c) 2013 Sam Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFSavingsGoalEditView : UIView
+
+@end
---
+++ CashFLow/SavingsGoal/CFSavingsGoalEditView.m
@@ -0,0 +1,22 @@
+//
+//  CFSavingsGoalEditView.m
+//  CashFLow
+//
+//  Created by Sam Watson on 22/07/13.
+//  Copyright (c) 2013 Sam Watson. All rights reserved.
+//
+
+#import "CFSavingsGoalEditView.h"
+
+@implementation CFSavingsGoalEditView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
